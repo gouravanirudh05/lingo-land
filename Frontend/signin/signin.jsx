4c8fd487d321f9df3dbf35d01e4f9d1d2417fec1
GouravAnirudh <146607605+gouravanirudh05@users.noreply.github.com>
2024-11-08 13:21:25 +0530

    Create signin.jsx

---
+++ Frontend/signin/signin.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="login-container">
+      <h1 className="login-title">Sign In</h1>
+      <div className="login-form">
+        <label className="login-label">Email</label>
+        <input type="email" className="login-input" placeholder="Email" />
+
+        <label className="login-label">Password</label>
+        <input type="password" className="login-input" placeholder="Password" />
+
+        <button className="sign-in-button">SIGN IN</button>
+      </div>
+      <div className="social-login">
+        <p>OR</p>
+        <button className="google-login">Google</button>
+      </div>
+    </div>
+  );
+};
+export default App;
