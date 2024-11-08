eac0c607c4085672d2093b193cd6331cd40c1c3f
GouravAnirudh <146607605+gouravanirudh05@users.noreply.github.com>
2024-11-08 08:55:02 +0530

    Create Button.jsx

---
+++ client/src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Button.css'; 
+
+const Button = () => {
+  return (
+    <button className="button">
+      <div className="state--layer">
+        <p className="text-1">Button</p>
+      </div>
+    </button>
+  );
+};
+
+export default Button;
